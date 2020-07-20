838a21f6dff713000d69ff41881dd7206e4ceb48
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-20 21:13:27 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -24,7 +24,7 @@ def consolidate_cart(cart)
     new_arr.push(value[:item])
   end 
 
-  #returns a hash with items in cart and quantity present using new_arr
+  #returns a hash with items in cart and respective quantity present using new_arr
  while counter < new_arr.length do 
    if (new_arr[counter] == new_arr[counter +1])
      target +=1
@@ -37,7 +37,9 @@ def consolidate_cart(cart)
   counter += 1
  end 
  
- counter = 0 
+ # Returns arr of hashes  
+ #iterates through created hash then iterates through cart_comparison to not mutate original cart 
+ #and if the final arr does not already include the current item in cart_comp push it into final_arr with :count key and value from new_hash
  new_hash.each do |key, value|
     puts !final_arr.any?{ |element| element[:item] == key}
     cart_comp.each do |item|
