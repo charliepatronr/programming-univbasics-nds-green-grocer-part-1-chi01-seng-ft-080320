b987de9ec5c767e979ddedb6adde55c8d995053b
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-20 20:55:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,62 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require "pry"
 
+def find_item_by_name_in_collection(name, collection)
+  target =nil
+  
+  collection.each do |value|
+    if(value[:item] == name)
+      target = value
+    end 
+  end 
+  target
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-end
-
-
+  new_hash = {}
+  counter =0
+  new_arr =[]
+  target = 1
+  final_arr = [ {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false} ]
   
+  cart.each do |value|
+    new_arr.push(value[:item])
+  end 
+
+ while counter < new_arr.length do 
+   if (new_arr[counter] == new_arr[counter +1])
+     target +=1
+   end 
+   
+   if(new_arr[counter] != new_arr[counter +1])
+     new_hash.store(new_arr[counter], target)
+      target = 1
+   end 
+  counter += 1
+ end 
+ 
+ counter = 0 
+ new_hash.each do |key, value|
+    puts final_arr[0].include?()
+  # while counter < cart.length do 
+  #   if (cart[counter][:item] == key && !final_arr.include?(:item => key))
+  #     final_arr.push(cart[counter])
+  #   end
+  #   counter +=1
+  # end 
+ end
+ puts final_arr
+
+ 
+end 
+
+
+consolidate_cart([
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false}
+])
+
+
+
